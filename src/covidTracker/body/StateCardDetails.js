12ce2c7a907f10831599f2fa5257a81e7e060cf3
hariprasanthzerobugz@gmail.com <hariprasanthzerobugz@gmail.com>
2022-08-02 17:48:27 +0530

    carousel

--- src/covidTracker/body/StateCardDetails.js
+++ src/covidTracker/body/StateCardDetails.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './stateCard.css'
 import StateDelta from './StateDelta'
 import StateDelta7 from './StateDelta7'
 import StateTotal from './StateTotal'
 
-const StateCardDetails = ({value: {total, delta, delta7}}) => {
+const StateCardDetails = ({ value: { total, delta, delta7 } }) => {
+
+    // * state
+    const [carousel, setCarousel] = useState(0)
+
+    // * carousel
+    const changeNextCarousel = () => setCarousel(carousel > 2 ? 1 : carousel+1)
+    const changePreviousCarousel = () => setCarousel(carousel === 0 ? 2 : carousel-1)
 
     return (
-        <div>
-            <StateTotal details={total} />
-            {/* <StateDelta details={delta} />
-            <StateDelta7 details={delta7} /> */}
+        <div className='d-flex justify-content-between'>
+            <div className='mt-5' onClick={e => changeNextCarousel()}>{'<'}</div>
+            {
+                carousel === 0 ? <StateTotal details={total} /> : carousel === 1 ? <StateDelta details={delta} /> : <StateDelta7 details={delta7} />
+            }
+            <div className='mt-5' onClick={e => changePreviousCarousel()}>{'>'}</div>
         </div>
     )
 }
