56e8f91fe8781228e6cdf4e7e16cd6c4a3509535
hariprasanthzerobugz@gmail.com <hariprasanthzerobugz@gmail.com>
2022-08-03 22:30:25 +0530

    dates

--- src/covidTracker/header/Topbar.js
+++ src/covidTracker/header/Topbar.js
@@ -1,42 +1,53 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { dateValue, searchValue } from '../../store/actions/actions';
+import './topbar.css'
 
 const Topbar = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    // * search
-    const [search, setSearch] = useState('')
-    const onChange = e => {
-        const value = e?.target?.value ?? null
-        setSearch(value)
+  // * search
+  const [search, setSearch] = useState('')
+  const onChange = e => {
+    const value = e?.target?.value ?? null
+    setSearch(value)
+  }
+  const checkForEnterKey = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      const value = e?.target?.value ?? null
+      setSearch(value)
+      dispatch(searchValue(value))
     }
-    const checkForEnterKey = (e) => {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            const value = e?.target?.value ?? null
-            setSearch(value)
-            dispatch(searchValue(value))
-        }
-    }
+  }
 
-    // * date
-    const [date, setDate] = useState('')
-    const onChangeDate = e => {
-        const value = e?.target?.value ?? null
-        setDate(value)
-        dispatch(dateValue(value))
-    }
+  // * date
+  const [date, setDate] = useState('')
+  const onChangeDate = e => {
+    const value = e?.target?.value ?? null
+    setDate(value)
+    dispatch(dateValue(value))
+  }
 
   return (
     <nav className="border-bottom d-flex header-bg justify-content-between mx-3 navbar navbar-dark" >
-        <div className="d-flex">
+
+      <div className="d-flex">
+
         <div className='form-label mt-2 mx-3' htmlFor='stateSearch'>State</div>
+
         <input type="search" placeholder="Search" className='form-control' id='stateSearch' value={search}
-         onChange={e => onChange(e)} onKeyUp={e => checkForEnterKey(e)} name="search" />
-        <input type="date" placeholder="Date" className='form-control mx-5' id='dateSearch' value={date}
-         onChange={e => onChangeDate(e)}  name="date" />
-         </div>
+          onChange={e => onChange(e)} onKeyUp={e => checkForEnterKey(e)} name="search" />
+
+        <div className='mx-5 text-center'>
+          <input type="date" placeholder="Date" className='form-control' id='dateSearch' value={date}
+            onChange={e => onChangeDate(e)} name="date" min="2020-01-30" max="2021-10-31" />
+            <span className='date-description'>FROM: 2020-01-30 - To: 2021-10-31</span>
+          {/* FROM: 2020-01-30 - To: 2021-10-31 */}
+        </div>
+
+      </div>
+
     </nav>
   )
 }
