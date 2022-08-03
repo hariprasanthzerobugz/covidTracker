40307b1437823be42af1ded053d336f884212f51
hariprasanthzerobugz@gmail.com <hariprasanthzerobugz@gmail.com>
2022-08-03 22:48:50 +0530

    topBar

--- src/covidTracker/header/Topbar.js
+++ src/covidTracker/header/Topbar.js
@@ -30,22 +30,19 @@ const Topbar = () => {
   }
 
   return (
-    <nav className="border-bottom d-flex header-bg justify-content-between mx-3 navbar navbar-dark" >
+    <nav className="border-bottom d-flex header-bg justify-content-start mx-3 navbar navbar-dark" >
 
       <div className="d-flex">
-
         <div className='form-label mt-2 mx-3' htmlFor='stateSearch'>State</div>
-
         <input type="search" placeholder="Search" className='form-control' id='stateSearch' value={search}
           onChange={e => onChange(e)} onKeyUp={e => checkForEnterKey(e)} name="search" />
+      </div>
 
-        <div className='mx-5 text-center'>
-          <input type="date" placeholder="Date" className='form-control' id='dateSearch' value={date}
-            onChange={e => onChangeDate(e)} name="date" min="2020-01-30" max="2021-10-31" />
-            <span className='date-description'>FROM: 2020-01-30 - To: 2021-10-31</span>
-          {/* FROM: 2020-01-30 - To: 2021-10-31 */}
-        </div>
-
+      <div className='mx-5 text-center'>
+        <input type="date" placeholder="Date" className='form-control' id='dateSearch' value={date}
+          onChange={e => onChangeDate(e)} name="date" min="2020-01-30" max="2021-10-31" />
+        <span className='date-description'>FROM: 2020-01-30 - To: 2021-10-31</span>
+        {/* FROM: 2020-01-30 - To: 2021-10-31 */}
       </div>
 
     </nav>
